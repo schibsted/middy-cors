ef6e1fed5a9db8956b89f946783304ddc96deb6e
Wojciech Iskra <wojtek@domeq.net>
2020-10-28 10:35:12 +0100

    Big refactor (middleware, tests, readme) (#2)
    * Refactor
    
    - rewrite tests
    - stop always adding 'access-control-allow-credentials' header
    - handle undefined handler.response object on error
    
    * Remove event from tests, not needed
    
    * Add README.md
    
    * 0.1.0
    
    * Add more keywords to package.json
    
    Co-authored-by: Wojciech Iskra <wojciech.iskra@schibsted.com>

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
-# middy-cors
-Adds CORS headers to the response (success and error)
+# Schibsted Middy CORS middleware
+
+#### CORS middleware for the middy framework, the stylish Node.js middleware engine for AWS Lambda
+
+
+This middleware sets HTTP CORS headers, necessary for making cross-origin requests, to the response object.
+
+Sets headers in `after` and `onError` phases.
+
+This is an alternative to [standard Middy cors handler](https://github.com/middyjs/middy/tree/master/packages/http-cors) with the following differences:
+- it allows you to add more CORS headers
+
+
+## Install
+
+To install this middleware you can use NPM:
+
+```bash
+npm install --save @schibsted/middy-cors
+```
+
+
+## Options
+
+- `allowedOrigins` (array) - list of allowed origins or `['*']` for allowing all origins
+- `exposeHeaders` (array) - list of headers to expose
+- `maxAge` (string) - value passed to `access-control-max-age` header
+- `credentials` (bool) - value passed to `access-control-allow-credentials` header 
+- `allowMethods` (array) - list of allowed HTTP methods
+- `allowHeaders` (array) - list of allowed HTTP headers
+
+
+## Sample usage
+
+```javascript
+const middy = require('@middy/core');
+const cors = require('@schibsted/middy-cors');
+
+const handler = middy(async () => ({
+        statusCode: 200,
+        body: JSON.stringify({ foo: 'bar' }),
+    }));
+
+handler
+  .use(cors({ allowedOrigins: ['https://www.vg.no', 'https://www.tek.no']}));
+
+// when Lambda runs the handler...
+handler({}, {}, (_, response) => {
+  expect(response).toEqual({
+    statusCode: 200,
+    headers: {
+        'access-control-allow-origin': 'https://www.vg.no, https://www.tek.no',
+    },
+    body: JSON.stringify({ foo: 'bar' }),
+  })
+})
+```
+
+
+## Contributing
+
+Everyone is very welcome to contribute to this repository. Feel free to [raise issues](https://github.com/schibsted/middy-cors/issues) or to [submit Pull Requests](https://github.com/schibsted/middy-cors/pulls).
--- index.test.js
+++ index.test.js
@@ -1,109 +1,137 @@
-test('Middleware returns all 2 handlers', () => {
-    const middleware = require('./index')();
-
-    expect(middleware.after).toBeInstanceOf(Function);
-    expect(middleware.onError).toBeInstanceOf(Function);
-});
+const middy = require('@middy/core');
+const createError = require('http-errors');
+const middleware = require('./index');
 
 test('Does nothing if has no config', async () => {
-    const middleware = require('./index')();
-
-    const payload = {
-        event: {
-            headers: {
-                host: 'localhost:3000',
-                connection: 'keep-alive',
-                'upgrade-insecure-requests': '1',
-                'user-agent':
-                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-                accept:
-                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,pl;q=0.7,nb;q=0.6,no;q=0.5',
-            },
-            httpMethod: 'GET',
-            path: '/foobar',
+    const handler = middy(async () => ({
+        statusCode: 200,
+        body: JSON.stringify({ foo: 'bar' }),
+        headers: {
+            someHeader: 'someValue',
         },
-        response: {
-            statusCode: 200,
+    }));
+
+    handler.use(middleware());
+
+    const response = await handler({}, {});
+    expect(response).toEqual({
+        statusCode: 200,
+        headers: {
+            someHeader: 'someValue',
         },
-    };
-
-    await middleware.after(payload);
-
-    expect(payload).toMatchSnapshot();
+        body: JSON.stringify({ foo: 'bar' }),
+    });
 });
 
 test('Adds CORS headers on success', async () => {
-    const middleware = require('./index')({
-        // This configuration is mirrored in serverless.cors.yml
-        allowedOrigins: ['https://www.vg.no', 'https://www.tek.no'],
-        exposeHeaders: ['x-my-header'],
-        maxAge: 2628000, // 1 month
-        credentials: true,
-        allowMethods: ['GET', 'POST'],
-        allowHeaders: ['Content-Type', 'Accept', 'X-Forwarded-For'],
+    const handler = middy(async () => ({
+        statusCode: 200,
+        body: JSON.stringify({ foo: 'bar' }),
+        headers: {
+            someHeader: 'someValue',
+        },
+    }));
+
+    handler.use(
+        middleware({
+            allowedOrigins: ['https://www.vg.no', 'https://www.tek.no'],
+            exposeHeaders: ['x-my-header'],
+            maxAge: 2628000, // 1 month
+            credentials: true,
+            allowMethods: ['GET', 'POST'],
+            allowHeaders: ['Content-Type', 'Accept', 'X-Forwarded-For'],
+        })
+    );
+
+    const response = await handler({}, {});
+    expect(response).toEqual({
+        statusCode: 200,
+        headers: {
+            'access-control-allow-credentials': true,
+            'access-control-allow-headers': 'Content-Type, Accept, X-Forwarded-For',
+            'access-control-allow-methods': 'GET, POST',
+            'access-control-allow-origin': 'https://www.vg.no, https://www.tek.no',
+            'access-control-expose-headers': 'x-my-header',
+            'access-control-max-age': 2628000,
+            someHeader: 'someValue',
+        },
+        body: JSON.stringify({ foo: 'bar' }),
     });
-
-    const payload = {
-        event: {
-            headers: {
-                host: 'localhost:3000',
-                connection: 'keep-alive',
-                'upgrade-insecure-requests': '1',
-                'user-agent':
-                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-                accept:
-                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,pl;q=0.7,nb;q=0.6,no;q=0.5',
-            },
-            httpMethod: 'GET',
-            path: '/foobar',
-        },
-        response: {
-            statusCode: 200,
-        },
-    };
-
-    await middleware.after(payload);
-
-    expect(payload).toMatchSnapshot();
 });
 
 test('Adds CORS headers on error', async () => {
-    const middleware = require('./index')({
-        // This configuration is mirrored in serverless.cors.yml
-        allowedOrigins: ['https://www.vg.no', 'https://www.tek.no'],
-        exposeHeaders: ['x-my-header'],
-        maxAge: 2628000, // 1 month
-        credentials: false,
-        allowMethods: ['GET', 'POST'],
-        allowHeaders: ['Content-Type', 'Accept', 'X-Forwarded-For'],
+    const handler = middy(async () => {
+        throw new createError.InternalServerError('whoops');
     });
 
-    const payload = {
-        event: {
-            headers: {
-                host: 'localhost:3000',
-                connection: 'keep-alive',
-                'upgrade-insecure-requests': '1',
-                'user-agent':
-                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
-                accept:
-                    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-                'accept-encoding': 'gzip, deflate, br',
-                'accept-language': 'en-GB,en;q=0.9,en-US;q=0.8,pl;q=0.7,nb;q=0.6,no;q=0.5',
+    handler.use(
+        middleware({
+            allowedOrigins: ['https://www.vg.no', 'https://www.tek.no'],
+            exposeHeaders: ['x-my-header'],
+            maxAge: 2628000, // 1 month
+            credentials: true,
+            allowMethods: ['GET', 'POST'],
+            allowHeaders: ['Content-Type', 'Accept', 'X-Forwarded-For'],
+        })
+    );
+
+    await expect(handler({}, {})).rejects.toEqual(
+        expect.objectContaining({
+            response: {
+                headers: {
+                    'access-control-allow-credentials': true,
+                    'access-control-allow-headers': 'Content-Type, Accept, X-Forwarded-For',
+                    'access-control-allow-methods': 'GET, POST',
+                    'access-control-allow-origin': 'https://www.vg.no, https://www.tek.no',
+                    'access-control-expose-headers': 'x-my-header',
+                    'access-control-max-age': 2628000,
+                },
             },
-            httpMethod: 'GET',
-            path: '/foobar',
-        },
-        response: {
-            statusCode: 500,
-        },
-    };
+        })
+    );
+});
 
-    await middleware.onError(payload);
+test('Keep headers already present in the response on error', async () => {
+    const handler = middy(async () => {
+        throw new createError.InternalServerError('whoops');
+    });
 
-    expect(payload).toMatchSnapshot();
+    // eslint-disable-next-line no-shadow
+    handler.onError(async (handler) => {
+        // eslint-disable-next-line no-param-reassign
+        handler.response = {
+            headers: {
+                someHeader: 'someValue',
+            },
+        };
+
+        return true;
+    });
+
+    handler.use(
+        middleware({
+            allowedOrigins: ['https://www.vg.no', 'https://www.tek.no'],
+            exposeHeaders: ['x-my-header'],
+            maxAge: 2628000, // 1 month
+            credentials: true,
+            allowMethods: ['GET', 'POST'],
+            allowHeaders: ['Content-Type', 'Accept', 'X-Forwarded-For'],
+        })
+    );
+
+    await expect(handler({}, {})).rejects.toEqual(
+        expect.objectContaining({
+            response: {
+                headers: {
+                    'access-control-allow-credentials': true,
+                    'access-control-allow-headers': 'Content-Type, Accept, X-Forwarded-For',
+                    'access-control-allow-methods': 'GET, POST',
+                    'access-control-allow-origin': 'https://www.vg.no, https://www.tek.no',
+                    'access-control-expose-headers': 'x-my-header',
+                    'access-control-max-age': 2628000,
+                    someHeader: 'someValue',
+                },
+            },
+        })
+    );
 });
