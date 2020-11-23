3019dbd251a3c5c304cd8680d59c4058de06fc14
Wojciech Iskra <wojtek@domeq.net>
2020-11-23 09:58:00 +0100

    Fix allowing all origins (#4)
    * 🐛 fix allowing all origins, add tests to verify
    
    * 0.2.1

--- index.test.js
+++ index.test.js
@@ -108,6 +108,49 @@ test('Adds CORS headers on success from allowed origin', async () => {
     });
 });
 
+test('Adds CORS headers on success when all origins allowed', async () => {
+    const handler = middy(async () => ({
+        statusCode: 200,
+        body: JSON.stringify({ foo: 'bar' }),
+        headers: {
+            someHeader: 'someValue',
+        },
+    }));
+
+    handler.use(
+        middleware({
+            allowedOrigins: ['*'],
+            exposeHeaders: ['x-my-header'],
+            maxAge: 2628000, // 1 month
+            credentials: true,
+            allowMethods: ['GET', 'POST'],
+            allowHeaders: ['Content-Type', 'Accept', 'X-Forwarded-For'],
+        })
+    );
+
+    const response = await handler(
+        {
+            headers: {
+                origin: 'https://www.tek.no',
+            },
+        },
+        {}
+    );
+    expect(response).toEqual({
+        statusCode: 200,
+        headers: {
+            'access-control-allow-credentials': true,
+            'access-control-allow-headers': 'Content-Type, Accept, X-Forwarded-For',
+            'access-control-allow-methods': 'GET, POST',
+            'access-control-allow-origin': 'https://www.tek.no',
+            'access-control-expose-headers': 'x-my-header',
+            'access-control-max-age': 2628000,
+            someHeader: 'someValue',
+        },
+        body: JSON.stringify({ foo: 'bar' }),
+    });
+});
+
 test('Adds CORS headers on error from disallowed origin', async () => {
     const handler = middy(async () => {
         throw new createError.InternalServerError('whoops');
@@ -156,6 +199,47 @@ test('Adds CORS headers on error from allowed origin', async () => {
     handler.use(
         middleware({
             allowedOrigins: ['https://www.vg.no', 'https://www.tek.no'],
+            exposeHeaders: ['x-my-header'],
+            maxAge: 2628000, // 1 month
+            credentials: true,
+            allowMethods: ['GET', 'POST'],
+            allowHeaders: ['Content-Type', 'Accept', 'X-Forwarded-For'],
+        })
+    );
+
+    await expect(
+        handler(
+            {
+                headers: {
+                    origin: 'https://www.tek.no',
+                },
+            },
+            {}
+        )
+    ).rejects.toEqual(
+        expect.objectContaining({
+            response: {
+                headers: {
+                    'access-control-allow-credentials': true,
+                    'access-control-allow-headers': 'Content-Type, Accept, X-Forwarded-For',
+                    'access-control-allow-methods': 'GET, POST',
+                    'access-control-allow-origin': 'https://www.tek.no',
+                    'access-control-expose-headers': 'x-my-header',
+                    'access-control-max-age': 2628000,
+                },
+            },
+        })
+    );
+});
+
+test('Adds CORS headers on error when all origins allowed', async () => {
+    const handler = middy(async () => {
+        throw new createError.InternalServerError('whoops');
+    });
+
+    handler.use(
+        middleware({
+            allowedOrigins: ['*'],
             exposeHeaders: ['x-my-header'],
             maxAge: 2628000, // 1 month
             credentials: true,
